90c7b9194090b458d35ea8b5dba89ffd260d02cb
vishwas955 <162691982+vishwas955@users.noreply.github.com>
2025-02-19 15:40:09 +0530

    Add files via upload

---
+++ backend/route/Membership.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Membershipcontroller = require('../controller/membership');
+const auth = require('../middlewares/auth');
+
+router.post('/add-membership',auth.IsAdminAuth,Membershipcontroller.newMembership);
+router.put('/update-membership/:id',auth.IsAdminAuth,Membershipcontroller.updateMembership);
+router.delete('/delete-membership/:id',auth.IsAdminAuth,Membershipcontroller.disableMembership);
+router.get('/get-membership',auth.IsAdminAuth,Membershipcontroller.getMembership);
+
+module.exports = router;
